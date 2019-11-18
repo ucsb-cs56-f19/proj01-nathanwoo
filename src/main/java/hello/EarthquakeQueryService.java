9ce89c4ff021784468308ea673112de93e8f7092
NathanWoo <nathanwuyang@gmail.com>
2019-11-18 12:34:58 -0800

    nw - added code to retrieve earthquake data from USGS web service

--- src/main/java/hello/EarthquakeQueryService.java
+++ src/main/java/hello/EarthquakeQueryService.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
