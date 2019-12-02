698e2882cc17a16054950434f1ffcbf9c97f9e35
NathanWoo <nathanwuyang@gmail.com>
2019-12-02 11:09:06 -0800

    nw - Added java bean code - Location.java that represents one row in the table

---
+++ src/main/java/earthquakes/entities/Location.java
@@ -0,0 +1,60 @@
+package earthquakes.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private long placeId;
+    private String name;
+    private double latitude;
+    private double longtitude;
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getPlaceId() {
+        return this.placeId;
+    }
+
+    public void setPlaceId(long placeId) {
+        this.placeId = placeId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLatitude() {
+        return this.latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongtitude() {
+        return this.longtitude;
+    }
+
+    public void setLongtitude(double longtitude) {
+        this.longtitude = longtitude;
+    }
+
+}
